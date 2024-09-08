91927464820a0c45ce628c8bdc147ee5a79caa01
HectorSchreiner <agenthector007@gmail.com>
2024-09-08 19:02:41 +0200

    cli

--- src/cli.rs
+++ src/cli.rs
@@ -1,36 +1,73 @@
-use clap::{Arg, Command};
+use clap::{Arg, ArgGroup, Command};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::path::Path;
 
-pub fn cli() {
-    let matches = Command::new("Rusty-Ripper")
-        .version("1.0")
-        .author("Hector Schousbo")
-        .about("Command line Bruteforce written in Rust")
-        .arg(
-            Arg::new("url")
-                .short('u')
-                .long("url")
-                .required(true)
-                .num_args(1)
-                .help("Specify attack url ex. https://www.example.com")
-        ).arg(
-            Arg::new("username")
-                .short('l')
-                .long("username")
-                .required(true)
-                .num_args(1)
-                .help("Specify username ex. admin")
-        ).arg(
-            Arg::new("password-wordlist")
-                .short('P')
-                .long("password-wordlist")
-                .required(true)
-                .num_args(1)
-                .help("Specify path for wordlist ex. /etc/wordlists/rockyou.txt")
-        ).get_matches();
+use crate::components::{Log, LogType, Logger};
+use crate::http_attack::http_post_attack;
 
-    let url = matches.get_one::<String>("url").unwrap();
-    let username = matches.get_one::<String>("username").unwrap();
-    let password_wordlist_path = matches.get_one::<String>("password-wordlist").unwrap();
+#[derive(Debug)]
+pub struct Cli {
+    pub address: String,
+    pub username: String,
+    pub password_wordlist: String,
+    pub mode: Mode
+}
+impl Cli {
+    pub fn new() -> Self {
+        let matches = Command::new("Rusty-Ripper")
+            .version("1.0")
+            .author("Hector Schousbo")
+            .about("Command line Bruteforce written in Rust")
+            .arg(
+                Arg::new("address")
+                    .short('a')
+                    .long("address")
+                    .required(true)
+                    .num_args(1)
+                    .help("Specify attack url or ip ex. https://www.example.com or 10.0.0.0")
+            ).arg(
+                Arg::new("username")
+                    .short('l')
+                    .long("username")
+                    .num_args(1)
+                    .help("Specify username ex. admin")
+            ).arg(
+                Arg::new("password-wordlist")
+                    .short('P')
+                    .long("password-wordlist")
+                    .required(true)
+                    .num_args(1)
+                    .help("Specify path for wordlist ex. /etc/wordlists/rockyou.txt")
+            ).arg(
+                Arg::new("mode")
+                    .short('m')
+                    .long("mode")
+                    .num_args(1)
+                    .help("Specify type of attack mode ex. http")
+            ).get_matches();
+
+        let address = matches.get_one::<String>("address").unwrap().to_string();
+        let username = matches.get_one::<String>("username").unwrap().to_string();
+        let password_wordlist = matches.get_one::<String>("password-wordlist").unwrap().to_string();
+        let mode_str = matches.get_one::<String>("mode").unwrap();
+
+        let mode: Mode = match mode_str.as_str() {
+            "http" => Mode::HTTP,
+            "ssh" => Mode::SSH,
+            _ => {
+                println!("Unknown attack mode: {}", mode_str);
+                std::process::exit(1);
+            }
+        };
+
+        Cli { address, username, password_wordlist, mode }
+    }
+
+}
+
+#[derive(Debug)]
+pub enum Mode {
+    HTTP,
+    SSH,
 }
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,27 @@ mod components;
 mod http_attack;
 mod cli;
 
+use components::{Log, LogType, Logger};
 use http_attack::http_post_attack;
+use cli::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let path = Path::new("/home/hector/Programming/Rust/rusty-ripper//src/wordlist.txt");
-    http_post_attack("https://google.com", path, "username").await?;    
+    let cli = Cli::new();
+    println!("{:?}", &cli);
+    run(&cli).await?;
+    Ok(())
+}
+
+async fn run(cli: &Cli) -> Result<(), Box<dyn Error>> {
+    match &cli.mode {
+        Mode::HTTP => {
+            http_post_attack(&cli.address, Path::new(&cli.password_wordlist.to_string()), &cli.username).await?;
+        }
+        _ => { 
+            let mut logger = Logger::new();
+            logger.log(Log::new("Error".to_string(), LogType::Error, true))
+        }
+    }
     Ok(())
 }
